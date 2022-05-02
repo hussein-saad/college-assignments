8e117a64abe2ae1618152b829e5e391e4715aba4
hussein-saad <hussein.saad2021@gmail.com>
2022-05-03 00:56:50 +0200

    Update

--- sheet3-solutions/student_record.cpp
+++ sheet3-solutions/student_record.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 // maximum size of class
-const int St_MAX = 3;
+const int St_MAX = 20;
 
 struct student{
     int id;
@@ -36,54 +36,88 @@ struct student{
 
 };
 
-struct system{
+struct student_record{
     // creat array of student struct
     student st[St_MAX];
+    int size;
+
+    student_record(){
+        size=0;
+    }
 
     // read all students
     void read_records(){
-        for (int i = 0; i < St_MAX; i++){
-            cout<<"STUDENT "<<i+1<<"\n";
-            cout<<"-----------------------\n";
-            st[i].add_student_record();
+
+        if (size >= St_MAX){
+            cout<<"Maximum number of students reached\n";
+            return;
         }
+
+        cout<<"STUDENT "<<size+1<<"\n";
+        cout<<"-----------------------\n";
+        st[size].add_student_record();
+        size++;
+    }
+
+    // check if the record is empty
+    int checker(){
+        return size;
     }
 
     // print all students records
     void print_records(){
-        for (int i = 0; i < St_MAX; i++){
+
+        if (!checker()){
+            cout<<"there is no student\n";
+            return;
+        }
+
+        for (int i = 0; i < size; i++){
             cout<<"STUDENT "<<i+1<<"\n";
             cout<<"-----------------------\n";
             st[i].print_student_record();
         }
     }
 
-    // get student by id
-    void find_student_by_id(){
-        int id;
-        cout<<"Enter the student id to search\n";
-        cin>>id;
-        bool flag = false;
-        for (int i = 0; i < St_MAX; i++){
+
+    // find student by id
+    bool find_student_by_id(int id){
+
+        for (int i = 0; i < size; i++){
             if (st[i].id == id){
-                cout<<"STUDENT "<<i+1<<"\n";
-                cout<<"-----------------------\n";
-                st[i].print_student_record();
-                flag  = true;
-                break;
+                return true;
             }
         }
+        return false;
+    }
+
+    // get student by id
+
+    void get_student_by_id(int id){
+
+        bool flag = false;
+        for (int i = 0; i < size; i++) {
+            if (st[i].id == id) {
+                cout << "STUDENT " << i + 1 << "\n";
+                cout << "-----------------------\n";
+                st[i].print_student_record();
+                flag = true;
+            }
+        }
+
         // prompt the user if student not found
-        if (!flag) cout<<"No student found\n";
+        if (!flag)
+            cout<<"No student found\n";
+
     }
 
 
     // sort students by their scores
     void sort_student_by_score(){
 
-        for (int i = 0; i < St_MAX; i++){
+        for (int i = 0; i < size; i++){
             double mn = st[i].total;
-            for (int j = i+1; j < St_MAX; j++){
+            for (int j = i+1; j < size; j++){
                 if (st[j].total < mn){
                     mn = st[j].total;
                     swap(st[i],st[j]);
@@ -94,6 +128,13 @@ struct system{
 
     // get student with minimum total score
     void get_min_score(){
+
+        if (!checker()){
+            cout<<"No student found\n";
+            return;
+        }
+
+        // sort students by their score in increasing order so the first has a minimum score
         sort_student_by_score();
         cout<<"Student with minimum score\n";
         st[0].print_student_record();
@@ -102,17 +143,128 @@ struct system{
 
     // get student with maximum total score
     void get_max_score(){
+
+        if (!checker()){
+            cout<<"No student found\n";
+            return;
+        }
+
+        // sort students by their score in increasing order so the last has a maximum score
         sort_student_by_score();
         cout<<"Student with maximum score\n";
-        st[St_MAX-1].print_student_record();
+        if (size > 0)
+            st[size-1].print_student_record();
+        else st[size].print_student_record();
         cout<<"-------------------------------\n";
     }
+
+    // select students by id
+    vector<int> select_students_by_id(){
+        vector<int> v; // vector to store selected students
+        int len;
+        // prompt user to enter the number of students to select
+        cout<<"how many students you want to calculate their average score\n";
+        cin>>len;
+
+        if (len > size){
+            cout<<"students are less than that\n";
+            return v;
+        }
+
+        int id;
+        // prompt user to enter the id of students to select
+        while (len){
+            cout<<"Enter the student id\n";
+            cin>>id;
+            if (find_student_by_id(id)) {
+                v.push_back(id), len--;
+            }
+            // prompt the user if student not found
+            else{
+                cout<<"there is no students with this id, please try again \n";
+            }
+        }
+        return v;
+    }
+
+    // calculate average score of selected students
+    void calculate_average_score(){
+        // get selected students
+        vector<int> v = select_students_by_id();
+        double sum = 0;
+        int len = v.size();
+        // calculate average score
+        for (int i = 0; i < len; i++){
+            for (int j = 0; j < St_MAX; j++){
+                if (st[j].id == v[i]){
+                    sum += st[j].total;
+                }
+            }
+        }
+        cout<<"Average score of selected students is "<<sum/len<<"\n";
+    }
+
+    // run the program
+    void run(){
+        int choice;
+        //keep running the program until user wants to exit
+        while (1){
+            cout<<"1. Add student records\n";
+            cout<<"2. View all student records\n";
+            cout<<"3. Calculate an average of a selected student’s scores\n";
+            cout<<"4. Show student who gets the max total score\n";
+            cout<<"5. Show student who gets the min total score\n";
+            cout<<"6. Find student by ID\n";
+            cout<<"7. Sort records by total scores\n";
+            cout<<"8. Exit\n";
+            cout<<"Enter your choice: ";
+            cin>>choice;
+            switch (choice) {
+                case 1:
+                    read_records();
+                    break;
+                case 2:
+                    print_records();
+                    break;
+                case 3:
+                    calculate_average_score();
+                    break;
+                case 4:
+                    get_max_score();
+                    break;
+                case 5:
+                    get_min_score();
+                    break;
+                case 6:
+                    int id;
+                    cout<<"Enter the student id\n";
+                    cin>>id;
+                    get_student_by_id(id);
+                    break;
+                case 7:
+                    sort_student_by_score();
+                    break;
+                case 8:
+                    return;
+                default:
+                    cout<<"Invalid choice, try again\n";
+                    break;
+            }
+        }
+    }
+
+
+
 };
 
 int main(){
-    struct system s;
-    s.read_records();
-    s.get_min_score();
-    s.get_max_score();
+
+//    freopen("input.txt", "r", stdin);
+
+    // create a new object
+    student_record sr;
+    // run the program
+    sr.run();
     return 0;
+
 }
--- sheet3-solutions/time.cpp
+++ sheet3-solutions/time.cpp
@@ -1,23 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct time{
+struct Time{
     int hour;
     int minute;
     int second;
 
-    time(){
+    // initialize time
+    Time(){
         hour = minute = second = 0;
     }
 
+    // read time
     void read(){
-        string str;
-        cout<<"Enter the time in 00:00:00 format";
-        cin>>str;
-        istringstream iss;
+        int h, m, s;
+        char first_char, second_char; // for (:) character
+        cin>>h>>first_char>>m>>second_char>>s;
+
+
+        // if the hour is greater than 24 or minute >= 60 or second >= 60, then it is invalid
+        // keep looping until the input is valid
+        while (h >= 24 || m >= 60 || s >= 60){
+            cout<<"Invalid time, try again"<<"\n";
+            cin>>h>>first_char>>m>>second_char>>s;
+        }
+        hour = h;
+        minute = m;
+        second = s;
     }
+
+    // convert time to seconds
+    int covert_to_seconds(){
+        return hour*3600 + minute*60 + second;
+    }
+
+    // convert seconds to time
+    void convert_back_to_time(int seconds){
+        hour = seconds/3600;// when divide by 3600, I get the hour
+
+        // when take the remainder of 3600 this will remove the complete cycles of hours
+        // and then divide the remainder by 60 to get the minutes
+        minute = (seconds%3600)/60;
+
+        // the remainder of the minutes will be the seconds
+        second = seconds%60;
+
+    }
+    // print time
+    void print(){
+        // if the hour is greater than 24 or minute >= 60 or second >= 60, then it is invalid
+        if (hour >= 24 || minute >= 60 || second >= 60){
+            cout<<"Invalid time summation"<<"\n";
+            return;
+        }
+        cout<<hour<<":"<<minute<<":"<<second<<"\n";
+    }
+
 };
 
 int main(){
 
+    Time t1, t2,result;
+    // read two time objects
+    cout<<"Enter the first time in 00:00:00 format: ";
+    t1.read();
+    cout<<"Enter the second time in 00:00:00 format:: ";
+    t2.read();
+    // convert them to seconds and store them in result
+    result.second = t1.covert_to_seconds() + t2.covert_to_seconds();
+    // convert the result back to time
+    result.convert_back_to_time(result.second);
+    // print the result
+    cout<<"The sum of the two times is: ";
+    result.print();
 }
+
+// 11:50:40
+// 10:40:10
